f3cfae7f0079fae69ecf7bec4ce7a9b97457380a
Abdul Moiz <amoizasif@khi.iba.edu.pk>
2020-10-08 18:29:55 +0500

    Before Pulling

--- front-end/src/Routes.js
+++ front-end/src/Routes.js
@@ -7,35 +7,31 @@ import PrivateRoute from "./auth/PrivateRoute";
 import Dashboard from "./user/UserDashboard";
 import AdminRoute from "./auth/AdminRoute";
 import AdminDashboard from "./user/AdminDashboard";
-
+import AdminComplaints from "./admin/AdminComplaints";
 
 const Routes = () => {
-    return (
-        <BrowserRouter>
-            <Switch>
-                <Route path="/" exact component={Home} />
-               
-                <Route path="/signin" exact component={Signin} />
-                <Route path="/signup" exact component={Signup} />
-                <PrivateRoute
-                    path="/user/dashboard"
-                    exact
-                    component={Dashboard}
-                />
-                <PrivateRoute
-                    path="/user/dashboard/:uid/:passwordHash"
-                    exact
-                    component={Dashboard}
-                />
-                <AdminRoute
-                    path="/admin/dashboard"
-                    exact
-                    component={AdminDashboard}
-                />
-               
-            </Switch>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={Home} />
+
+        <Route path="/signin" exact component={Signin} />
+        <Route path="/signup" exact component={Signup} />
+        <PrivateRoute path="/user/dashboard" exact component={Dashboard} />
+        <PrivateRoute
+          path="/user/dashboard/:uid/:passwordHash"
+          exact
+          component={Dashboard}
+        />
+        <AdminRoute path="/admin/dashboard" exact component={AdminDashboard} />
+        <AdminRoute
+          path="/admin/complaints"
+          exact
+          component={AdminComplaints}
+        />
+      </Switch>
+    </BrowserRouter>
+  );
 };
 
 export default Routes;
